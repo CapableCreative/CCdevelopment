5a41aed90d02e6f88b4801ca340bc17291acff49
Stephen Fox <stephen@capablecreative.com>
2019-07-23 22:08:48 -0500

    This is the first go at prototyping the single-canvas portfolio site concept

---
+++ assets/capable.js
@@ -0,0 +1,8 @@
+function viewport2() {
+    var v2Slide = document.getElementById('viewport2');
+    v2Slide.style.marginLeft = "-50%";
+}
+function viewport1() {
+    var v1Slide = document.getElementById('viewport2');
+    v1Slide.style.marginLeft = "0%";
+}
